703260d710feba703eebd8cd2ac50a5df43d8296
becauseiliketolike <131786997+becauseiliketolike@users.noreply.github.com>
2023-05-14 18:30:11 +0545

    lab5 reverted through online

--- script.js
+++
@@ -1,6 +0,0 @@
-const searchBoxIcon = document.querySelector(".search-icon");
-const searchBoxInput = document.querySelector(".search-box input");
-
-searchBoxIcon.addEventListener("click", () => {
-  searchBoxInput.classList.toggle("active");
-});
