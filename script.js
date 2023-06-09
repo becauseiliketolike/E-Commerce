39c0017cad2816321769b8d591acc56aa908ebf1
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-06-09 05:23:44 -0700

    slider fixes and image remake

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const searchBoxIcon = document.querySelector(".search-icon");
 const searchBoxInput = document.querySelector(".search-box input");
-const carSlider = document.querySelector(".car-slider");
+const carSliderImg = document.querySelector(".car-slider img");
 
 searchBoxIcon.addEventListener("click", () => {
   searchBoxInput.classList.toggle("active");
 });
 
 let i = 0;
-const img = document.createElement("img");
-img.src = `./images/car-${(i + 1) % 6}.jpg`;
-img.alt = `Car ${(i + 1) % 6}`;
-carSlider.appendChild(img);
-i++;
-
 setInterval(() => {
-  img.src = `./images/car-${(i + 1) % 6}.jpg`;
-  img.alt = `Car ${(i + 1) % 6}`;
-  carSlider.appendChild(img);
+  if (i == 4) i = -1;
   i++;
-  if (i == 5) i = 0;
-}, 2000);
-// if (i == 4) i = 0;
+  carSliderImg.src = `./images/car-${i % 5}.jpg`;
+  carSliderImg.alt = `Car ${i % 5}`;
+}, 3000);
