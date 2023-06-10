14a362ce01f3d23f9675a044281c503715169183
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-06-09 20:39:19 -0700

    Slider completed

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const searchBoxIcon = document.querySelector(".search-icon");
 const searchBoxInput = document.querySelector(".search-box input");
 const carSliderImg = document.querySelector(".car-slider img");
+const faArrowLeft = document.querySelector(".fa-arrow-left");
+const faArrowRight = document.querySelector(".fa-arrow-right");
 
 searchBoxIcon.addEventListener("click", () => {
   searchBoxInput.classList.toggle("active");
 });
 
 let i = 0;
-setInterval(() => {
-  if (i == 4) i = -1;
-  i++;
-  carSliderImg.src = `./images/car-${i % 5}.jpg`;
-  carSliderImg.alt = `Car ${i % 5}`;
-}, 3000);
+let sliderInterval;
+const sliderImageLength = 7;
+
+const startInterval = () => {
+  sliderInterval = setInterval(() => {
+    if (i == sliderImageLength - 1) i = -1;
+    i++;
+    updateSlider(i);
+  }, 2500);
+};
+
+const updateSlider = (x) => {
+  carSliderImg.src = `./images/slide-${x % sliderImageLength}.jpg`;
+  carSliderImg.alt = `Image ${x % sliderImageLength}`;
+  clearInterval(sliderInterval);
+  startInterval();
+};
+
+faArrowLeft.addEventListener("click", () => {
+  i == 0 ? (i = sliderImageLength - 1) : (i = (i - 1) % sliderImageLength);
+  updateSlider(i);
+});
+
+faArrowRight.addEventListener("click", () => {
+  i = (i + 1) % sliderImageLength;
+  updateSlider(i);
+});
+
+startInterval();
